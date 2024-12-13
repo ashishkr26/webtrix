b22bff003fe2eb0a5a4ac6a49065ceeb4f579efc
ashishkr26 <ashish.dypcoe@gmail.com>
2024-12-13 14:17:06 +0530

    main container

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,26 +6,28 @@ import CloseIcon from "@mui/icons-material/Close";
 const Header = () => {
   const [sideIcon, setSideIcon] = useState(false);
   return (
-    <div className="bg-yellow-400 text-white opacity-70ont-semibold p-2 lg:text-lg md:text-base sm:text-sm shadow-lg">
-      <ul className="flex flex-row">
-        <li onClick={() => setSideIcon(!sideIcon)} className="p-2">
+    <div className="bg-yellow-400 text-white opacity-70ont-semibold p-2 lg:text-lg md:text-base sm:text-sm shadow-lg flex justify-between">
+      <ul className="flex flex-row font-semibold">
+        <li onClick={() => setSideIcon(!sideIcon)} className="p-2 hover:scale-110 transition-all duration-300 ease-in-out">
           {sideIcon ? (
             <MenuIcon sx={{ fontSize: 32 }} />
           ) : (
             <CloseIcon sx={{ fontSize: 32 }} />
           )}
         </li>
-        <li className="p-2 ">Home</li>
-        <li className="p-2">Products</li>
-        <li className="p-2">Services</li>
-        <li className="px-10 py-1">
+        <li className="p-2 hover:scale-110 transition-all duration-300 ease-in-out">Home</li>
+        <li className="p-2 hover:scale-110 transition-all duration-300 ease-in-out">Products</li>
+        <li className="p-2 hover:scale-110 transition-all duration-300 ease-in-out">Services</li>
+        <li className="px-10 py-1 hover:scale-105 transition-all duration-300 ease-in-out">
           <input
-            className="lg:w-96 md:w-56 sm:w-32 p-2 flex flex-grow justify-center items-center rounded-full"
+            className="lg:w-[500px] md:w-56 sm:w-32 p-2 flex flex-grow justify-center items-center rounded-full"
             type="text"
             placeholder="Search"
           />
         </li>
+
       </ul>
+      <div><button className="bg-gray-500 p-2 mt-1 rounded-md border border-white font-semibold hover:scale-105 transition-all duration-300 ease-in-out"> Demo On Demand</button></div>
     </div>
   );
 };
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,24 @@
 import React from "react";
+import newbg from "./newbg.jpg";
 
 const MainContainer = () => {
   return (
-    <div className="m-2 p-2 flex flex-col w-auto items-start bg-gray-50">
+    <div
+      style={{
+        backgroundImage: `url(${newbg})`, // Correct syntax for inline styles
+        backgroundSize: "cover", // Ensures the image covers the entire div
+        backgroundPosition: "center", // Centers the image
+        
+      }}
+      className=" p-2 flex flex-col w-auto items-start bg-gray-50"
+    >
       <div className="p-2 m-2 bg-yellow-400 rounded-r-full h-16">
-        <span className="bg-gray-500 relative top-2 p-2 text-2xl rounded-r-full font-semibold text-white ">
+        <span className="bg-gray-500 relative top-2 p-2 text-2xl rounded-r-full font-semibold text-white">
           Empower Your Business with Our IT Solutions
         </span>
       </div>
       <div className="p-2 m-2 bg-yellow-400 rounded-r-full h-16">
-        <span className="bg-gray-500 relative top-2 p-2 text-2xl rounded-r-full font-semibold text-white ">
+        <span className="bg-gray-500 relative top-2 p-2 text-2xl rounded-r-full font-semibold text-white">
           Website Development
         </span>
       </div>
@@ -19,13 +28,13 @@ const MainContainer = () => {
         </span>
       </div>
       <div className="p-2 m-2 bg-yellow-400 rounded-r-full h-16">
-        <span className="bg-gray-500 relative top-2 p-2 text-2xl rounded-r-full font-semibold text-white ">
-          SEO- Search Engine Optimization
+        <span className="bg-gray-500 relative top-2 p-2 text-2xl rounded-r-full font-semibold text-white">
+          SEO - Search Engine Optimization
         </span>
       </div>
       <div className="p-2 m-2 bg-yellow-400 rounded-r-full h-16">
-        <span className="bg-gray-500 relative top-2 p-2 text-2xl rounded-r-full font-semibold text-white ">
-          Mobile App Developemt
+        <span className="bg-gray-500 relative top-2 p-2 text-2xl rounded-r-full font-semibold text-white">
+          Mobile App Development
         </span>
       </div>
     </div>
