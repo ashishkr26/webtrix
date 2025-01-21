8993e3e37e1da66f687136b43cb8e3af347fbd66
ashishkr26 <ashish.dypcoe@gmail.com>
2025-01-21 17:19:57 +0530

    info cards updated

--- src/components/InfoCards.js
+++ src/components/InfoCards.js
@@ -3,6 +3,7 @@ import AssuredWorkloadIcon from "@mui/icons-material/AssuredWorkload";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import GroupsIcon from "@mui/icons-material/Groups";
 import StarsIcon from "@mui/icons-material/Stars";
+import HubOutlinedIcon from '@mui/icons-material/HubOutlined';
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 
 const InfoCards = () => {
@@ -11,42 +12,44 @@ const InfoCards = () => {
       <div className="my-1 grid grid-flow-col grid-cols-11">
         <div className=" col-span-4 border border-gray-600 p-8 bg-clay flex flex-col items-center">
           <span>
-            <AssuredWorkloadIcon sx={{fontSize:72}}/>
+            <AssuredWorkloadIcon sx={{ fontSize: 72, color: "white" }} />
           </span>
-          <span>Established 2025</span>
+          <span className="text-white">Established 2025</span>
         </div>
         <div className=" col-span-3 border border-gray-600 p-8 bg-gray-700 flex flex-col items-center">
           <span>
-            <AdminPanelSettingsIcon sx={{fontSize:72}}/>
+            <AdminPanelSettingsIcon
+              sx={{ fontSize: 72, color: "whitesmoke" }}
+            />
           </span>
-          <span>40+Users</span>
+          <span className="text-white">40+Users</span>
         </div>
-        <div className=" col-span-4 border border-gray-600 p-8 bg-slate-200 flex flex-col items-center">
+        <div className=" col-span-4 border border-gray-600 p-8 bg-slate-500 flex flex-col items-center">
           <span>
-            <GroupsIcon sx={{fontSize:72}}/>
+            <GroupsIcon sx={{ fontSize: 72, color: "whitesmoke" }} />
           </span>
-          <span>Team of 20+</span>
+          <span className="text-white">Team of 20+</span>
         </div>
       </div>
 
       <div className=" grid grid-flow-col grid-cols-12">
-        <div className=" col-span-3 border border-gray-600 p-8 bg-amber-200 flex flex-col items-center">
+        <div className=" col-span-3 border border-gray-600 p-8 bg-indigo-300 flex flex-col items-center">
           <span>
-            <GroupsIcon sx={{fontSize:72}}/>
+            <HubOutlinedIcon sx={{ fontSize: 72, color: "white" }} />
           </span>
-          <span>7+ Years Experience</span>
+          <span className="text-white">7+ Years Experience</span>
         </div>
         <div className=" col-span-6 border border-gray-600 p-8 bg-zinc-500 flex flex-col items-center">
           <span>
-            <StarsIcon sx={{fontSize:72}}/>
+            <StarsIcon sx={{ fontSize: 72, color: "whitesmoke" }} />
           </span>
-          <span>4.9 ratings on Google</span>
+          <span className="text-white">4.9 ratings on Google</span>
         </div>
-        <div className=" col-span-3 border border-gray-600 p-8 bg-neutral-600 flex flex-col items-center">
+        <div className=" col-span-3 border border-gray-500 p-8 bg-neutral-600 flex flex-col items-center">
           <span>
-            <EmojiEventsIcon sx={{fontSize:72}}/>
+            <EmojiEventsIcon sx={{ fontSize: 72, color: "white" }} />
           </span>
-          <span>10+ Awards</span>
+          <span className="text-white">10+ Awards</span>
         </div>
       </div>
     </div>
