492e953f1cb9762ef9e043c6575b95daf87fbda7
ashishkr26 <ashish.dypcoe@gmail.com>
2025-03-09 23:30:33 +0530

    login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,6 +10,11 @@ const Login = () => {
               placeholder="Your Name"
             
             />
+            <input className="my-2 p-2 border border-gray-700 rounded-md text-sm md:text-base xs:text-xs"
+            type="password"
+            placeholder='Password'
+            />
+            <div><button>Submit</button></div>
       </div>
     </div>
   )
